5007f4790232917b3f99f7c301aaedd16585b030
NotXia <35894453+NotXia@users.noreply.github.com>
2024-07-17 20:32:13 +0200

    Add trainer utility

---
+++ src/utils/Trainer.py
@@ -0,0 +1,48 @@
+from tqdm.auto import tqdm
+from layers import Layer
+from losses import Loss
+from optimizers import Optimizer
+from data import DataLoader
+from differentiation import Node
+
+
+
+class Trainer:
+    def __init__(self, 
+        model: Layer,
+        loss: Loss,
+        optimizer: Optimizer,
+        train_data: DataLoader,
+        val_data: DataLoader|None = None
+    ):
+        self.model = model
+        self.loss = loss
+        self.optimizer = optimizer
+        self.train_data = train_data
+        self.val_data = val_data
+        self.tot_epochs = 0
+
+
+    def train(self, epochs: int):
+        for epoch in range(epochs):
+            epoch_train_loss = 0
+
+            for batch in self.train_data:
+                self.optimizer.zero_grad()
+                batch_loss = Node(0)
+
+                for input, label in batch:
+                    pred = self.model(input)
+                    batch_loss += self.loss(pred, label)
+                
+                batch_loss = batch_loss / len(batch)
+                batch_loss.backward()
+                self.optimizer.step()
+
+                epoch_train_loss += batch_loss.value[0, 0]
+
+            epoch_train_loss = epoch_train_loss / len(self.train_data)
+            print(f"Epoch {epoch+1} | train_loss: {epoch_train_loss:.4f}")
+            self.tot_epochs += 1
+        
+        return self.model
