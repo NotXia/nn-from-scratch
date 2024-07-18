ce22f94c9ec466e71b54def373bea8436700373d
NotXia <35894453+NotXia@users.noreply.github.com>
2024-07-18 10:43:36 +0200

    Fix linear layer shapes

--- src/layers/Linear.py
+++ src/layers/Linear.py
@@ -9,7 +9,7 @@ class Linear(Layer):
     def __init__(self, in_dim: int, out_dim: int, initializer: str="xavier"):
         initializer_fn = getInitializer(initializer)
         self.weights = Node( initializer_fn(in_dim, out_dim) )
-        self.biases = Node( np.zeros((out_dim, )) )
+        self.biases = Node( np.zeros((out_dim, 1)) )
 
 
     def __call__(self, batch: list[Node]):
