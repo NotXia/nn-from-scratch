56fe53936c3cab5d6787f61743d31dedee28c62b
NotXia <35894453+NotXia@users.noreply.github.com>
2024-07-26 18:57:48 +0200

    Add label smoothing

--- src/losses/CategoricalCrossEntropy.py
+++ src/losses/CategoricalCrossEntropy.py
@@ -5,17 +5,21 @@ import numpy as np
 
 
 class CategoricalCrossEntropy(Loss):
-    def __init__(self):
-        pass
+    def __init__(self, label_smoothing: float|None=None):
+        self.label_smoothing = label_smoothing
 
 
     def __call__(self, preds: list[Node], labels: list[Node]) -> Node:
         batch_loss = Node(0)
         
         for pred, label in zip(preds, labels):
-            mask = np.zeros_like(pred.value)
-            mask[np.argmax(label)] = 1
-            batch_loss += -Node.sum( Node(mask) * Node.log(pred) )
+            label = np.expand_dims(label, axis=-1)
+            if self.label_smoothing is not None:
+                smooth_factor = self.label_smoothing / len(label)
+                label[label == 0] = smooth_factor
+                label[label == 1] = 1 - smooth_factor*(len(label)-1)
+
+            batch_loss += -Node.sum( Node(label) * Node.log(pred) )
         batch_loss = batch_loss / len(preds)
 
         return batch_loss
