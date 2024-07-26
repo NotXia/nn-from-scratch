9cbe14c9acc15a5a9a1117d964b4019f9acc3434
NotXia <35894453+NotXia@users.noreply.github.com>
2024-07-26 18:54:56 +0200

    Add flatten layer

--- src/layers/__init__.py
+++ src/layers/__init__.py
@@ -5,3 +5,4 @@ from .composition.Module import *
 from .Parameters import *
 from .vision.Conv2D import * 
 from .vision.MaxPool import * 
+from .vision.Flatten import * 
---
+++ src/layers/vision/Flatten.py
@@ -0,0 +1,25 @@
+from ..Layer import Layer
+import numpy as np
+from differentiation import Node
+from ..Parameters import Parameters
+
+
+
+class Flatten(Layer):
+    def __init__(self):
+        pass
+
+
+    def __call__(self, batch: list[Node]|list[np.ndarray]) -> list[Node]:
+        if not isinstance(batch[0], Node): batch = [ Node(x) for x in batch ]
+        assert all([ x.value.shape == batch[0].value.shape for x in batch ])
+
+        return [x.flatten().reshape((-1, 1)) for x in batch]
+    
+
+    def zero_grad(self):
+        pass
+
+    
+    def parameters(self) -> list[Parameters]:
+        return []
